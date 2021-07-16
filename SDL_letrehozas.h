ffc6ed16439058630104a3e0765289ff22f01580
bbe11111 <41309137+Benedek-Brand@users.noreply.github.com>
2021-07-17 00:07:44 +0200

    Delete SDL_letrehozas.h

--- SDL_letrehozas.h
+++
@@ -1,9 +0,0 @@
-#ifndef NHZ_SDL_LETREHOZAS_H
-#define NHZ_SDL_LETREHOZAS_H
-
-#include "SDL.h"
-#include "debugmalloc.h"
-
-void sdl_init(SDL_Window **pwindow, SDL_Renderer **prenderer,int Width,int Height);
-
-#endif //NHZ_SDL_LETREHOZAS_H
